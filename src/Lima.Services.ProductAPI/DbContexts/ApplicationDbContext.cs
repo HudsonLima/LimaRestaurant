ab2400a6af9b893ada0e5c16339ce613284bae1d
Hudson Lima <hudsonlima.ti@gmail.com>
2022-07-16 23:10:24 -0300

    Add DbContext and Product Entity

---
+++ src/Lima.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lima.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lima.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
