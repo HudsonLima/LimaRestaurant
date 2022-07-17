2c52c8af567523271f623314c3acd0f499a8ec50
Hudson Lima <hudsonlima.ti@gmail.com>
2022-07-17 14:16:27 -0300

    Configure Mapping

---
+++ src/Lima.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Lima.Services.ProductAPI.Models;
+using Lima.Services.ProductAPI.Models.Dto;
+
+namespace Lima.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
