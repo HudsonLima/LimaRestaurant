e1aae21f32757b3a21f5e72b3e2b258732f9f7ab
Hudson Lima <hudsonlima.ti@gmail.com>
2022-07-17 14:10:56 -0300

    Setup Product Repo Interface

---
+++ src/Lima.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Lima.Services.ProductAPI.Models.Dto;
+
+namespace Lima.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+
+        Task<ProductDto> GetProducById(int productId);
+
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+
+        Task<bool> DeleteProduct(int productId);
+    }
+}
