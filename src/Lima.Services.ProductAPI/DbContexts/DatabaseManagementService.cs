92f2aea4e0aa4395fdb4fd90b079e706f2c82f5c
Hudson Lima <hudsonlima.ti@gmail.com>
2022-07-17 21:53:47 -0300

    Enable Migrations on sql server container

---
+++ src/Lima.Services.ProductAPI/DbContexts/DatabaseManagementService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lima.Services.ProductAPI.DbContexts
+{
+    public static class DatabaseManagementService
+    {
+        public static void MigrationInitialization(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.Migrate();
+            }
+        }
+    }
+}
--- src/Lima.Services.ProductAPI/Program.cs
+++ src/Lima.Services.ProductAPI/Program.cs
@@ -30,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+DatabaseManagementService.MigrationInitialization(app);
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
