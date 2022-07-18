0c6936a1b89607116b96e892476f42ac6310e1b7
Hudson Lima <hudsonlima.ti@gmail.com>
2022-07-18 00:52:46 -0300

    [Lima.Web] Setup API Service

--- src/Lima.Web/Program.cs
+++ src/Lima.Web/Program.cs
@@ -1,6 +1,14 @@
+using Lima.Web;
+using Lima.Web.Services;
+using Lima.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHttpClient<IProductService, ProductService>();
+StaticDetails.ProductAPIBase = builder.Configuration["ServiceUrls:ProductApi"];
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
